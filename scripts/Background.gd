9a99bfcf1495208b9532516fc634daee8b4925ef
Nicolas Mai <nicognito@users.noreply.github.com>
2023-08-29 00:15:25 -0700

    Reset background parallax effect

--- scripts/Background.gd
+++ scripts/Background.gd
@@ -6,3 +6,5 @@ extends ParallaxBackground
 
 func _process(delta):
 	sprite.region_rect.position += delta * Vector2(scroll_speed, scroll_speed)
+	if sprite.region_rect.position >= Vector2(256, 256):
+		sprite.region_rect.position = Vector2.ZERO
